a8c6580db2131879dffae3107cacddbe1de6dffc
alvaro911 <algp232@hotmail.com>
2017-06-07 10:26:54 -0600

    built array of cities and car models to be scraped

---
+++ config.js
@@ -0,0 +1,2 @@
+const DB_URL_PROD = 'mongodb://localhost/car_finder_app-prod'
+const DB_URL_DEV = 'mongodb://localhost/car_finder_app-dev'
---
+++ cron-job.js
@@ -0,0 +1,5 @@
+const cron = require('cron')
+const mongoose = require('mongoose')
+
+const coCities = [boulder, cosprings, denver, eastco, fortcollins, rockies, pueblo, westslope]
+const nissan = [toyota, nissan, ford, audi, ferrari, bmw, volkswagen, hyundai, mitsubishi, honda, buick, chevrolet, jeep, dodge]
